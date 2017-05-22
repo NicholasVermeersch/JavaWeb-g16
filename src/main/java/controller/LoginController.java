9019c6a45f653ff309d71d6666466f5eb77c90f8
DylanVanPaemel <dylan.vanpaemel.w1044@student.hogent.be>
2017-05-22 11:41:48 +0200

    Logincontroller aanpassen

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -14,45 +14,51 @@ import org.springframework.web.bind.annotation.RequestParam;
 import service.CursistDao;
 import service.GenericDaoJpa;
 import service.JpaCursistDao;
- 
+
 @Controller
-public class LoginController {
-    
-@Autowired
+public class LoginController
+{
+
+    @Autowired
     private CursistDao cursistDao;
 
     @RequestMapping(value = "/welcome", method = RequestMethod.GET)
-    public String printWelcome(Model model, Principal principal) {
-        model.addAttribute("username", cursistDao.getByUsername(principal.getName()).getName());       
-        model.addAttribute("message", "Spring Security Custom Form example");
+    public String printWelcome(Model model, Principal principal)
+    {
+        model.addAttribute("username", cursistDao.getByUsername(principal.getName()).getName());
+        model.addAttribute("message", "Give a day peerassessment");
         List<Cursist> cursisten = new ArrayList<Cursist>();
         Cursist cursist = cursistDao.getByUsername(principal.getName());
-       /* cursisten.add(new Cursist("Tjeefken"));
-    cursisten.add(new Cursist("fideel"));
-    cursisten.add(new Cursist("Dolfken"));
-    cursisten.add(new Cursist("Theofiel"));
-    cursisten.add(new Cursist("Juul Cesar"));*/
-        
-      //Groep groep= new Groep("de fonzen",cursisten);
-      cursisten = cursist.getGroep().getCursists();
-      model.addAttribute("groep",cursist.getGroep().getName());
-      String cursistString = "";
-      for(Cursist curs:cursisten){
-          cursistString +="<li>"+curs.getName()+"</li>";
-      }
-      model.addAttribute("cursisten",String.format("<ul>%s</ul>",cursistString));
-      //model.addAttribute("ingediend",cursist.getIngediend());
+        cursisten.add(new Cursist("Tjeefken"));
+        cursisten.add(new Cursist("fideel"));
+        cursisten.add(new Cursist("Dolfken"));
+        cursisten.add(new Cursist("Theofiel"));
+        cursisten.add(new Cursist("Juul Cesar"));
+
+        //Groep groep= new Groep("de fonzen",cursisten);
+        cursisten = cursist.getGroep().getCursists();
+        model.addAttribute("groep", cursist.getGroep().getName());
+        String cursistString = "";
+        for (Cursist curs : cursisten)
+        {
+            cursistString += "<li>" + curs.getName() + "</li>";
+        }
+        model.addAttribute("cursisten", String.format("<ul>%s</ul>", cursistString));
+        //model.addAttribute("ingediend",cursist.getIngediend());
         return "hello";
     }
-    
+
     @RequestMapping(value = "/login")
     public String login(@RequestParam(value = "error", required = false) String error,
-            @RequestParam(value = "logout", required = false) String logout, Model model) {
+            @RequestParam(value = "logout", required = false) String logout, Model model)
+    {
 
-        if (error != null) {
+        if (error != null)
+        {
             model.addAttribute("error", "Invalid username and password!");
         }
-        if (logout != null) {
+        if (logout != null)
+        {
             model.addAttribute("msg", "You've been logged out successfully.");
         }
         return "login";
